67ef80e148323da522ef3ef036d97609089d3877
talfik2 <emregondermez@gmail.com>
2022-01-29 16:07:10 +0100

    29.01.2022

---
+++ src/test/java/practice/Q03.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Q03 {
+    // ...Exercise3...
+    // go to url : https://www.techlistic.com/p/selenium-practice-form.html
+    //fill the firstname
+    //fill the firstname
+    //check the gender
+    //check the experience
+    //fill the date
+    //choose your profession -> Automation Tester
+    //choose your tool -> Selenium Webdriver
+    //choose your continent -> Antartica
+    //choose your command  -> Browser Commands
+    //click submit button
+
+
+
+}
